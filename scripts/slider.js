e9c945962e05f7c391d5cca270e83b095597e4a3
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-06-05 01:34:47 +0300

    slider styles

---
+++ scripts/slider.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    dots: true,
+    slidesToshow: 4,
+    slidesToScroll: 1,
+    speed: 1000,
+    // infinite: false,
+    initialSlide: 2,
+    variableWidth: true,
+    centerMode: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    waitForAnimate: false,
+    responsive: [
+      {
+        breakpoint: 720,
+        settings: {
+          infinite: false,
+        },
+      },
+    ],
+  });
+});

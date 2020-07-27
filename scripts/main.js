092cb1a98de78d831b2afab61923b6d7754af6e5
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-07-27 12:44:58 +0300

    small changes

--- scripts/main.js
+++ scripts/main.js
@@ -17,3 +17,30 @@ $(function () {
   });
 });
 
+$(document).ready(function () {
+  $(".slider").slick({
+    dots: true,
+    centerPadding: "0px",
+    slidesToshow: 3,
+    slidesToScroll: 1,
+    speed: 400,
+    // infinite: false,
+    initialSlide: 1,
+    variableWidth: true,
+    centerMode: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    waitForAnimate: false,
+    lazyLoad: 'ondemand',
+    responsive: [
+      {
+        breakpoint: 720,
+        settings: {
+          infinite: false,
+          arrows: false,
+          centerMode: true,
+        },
+      },
+    ],
+  });
+});
